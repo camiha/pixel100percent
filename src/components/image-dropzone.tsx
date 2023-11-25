12480669cb188167464b05c4f36d64c48670e23b
takumi <hello.camiha@gmail.com>
2023-11-25 23:46:32 +0900

    set spinner

--- src/components/image-dropzone.tsx
+++ src/components/image-dropzone.tsx
@@ -1,4 +1,5 @@
-import { Flex, Icon, Text } from "@chakra-ui/react";
+import { Flex, Icon, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import Dropzone from "react-dropzone";
 import { SlPicture } from "react-icons/sl";
 
@@ -7,22 +8,32 @@ export const ImageDropzone = ({
 }: {
 	setImageData: (imageData: string) => void;
 }) => {
+	const [isLoading, setIsLoading] = useState(false);
+
 	const handleOnDrop = <T extends File,>(acceptedFiles: T[]) => {
+		setIsLoading(true);
+
 		const acceptedFile = acceptedFiles[0];
-		if (!acceptedFile.type.includes("image/")) return;
+		if (!acceptedFile.type.includes("image/")) {
+			setIsLoading(false);
+			return;
+		}
 		const reader = new FileReader();
 		reader.readAsDataURL(acceptedFile);
 		reader.onload = () => {
-			if (typeof reader.result !== "string") return;
+			if (typeof reader.result !== "string") {
+				setIsLoading(false);
+				return;
+			}
 			setImageData(reader.result);
+			setIsLoading(false);
 		};
 	};
 
 	return (
-		<Dropzone onDrop={handleOnDrop}>
-			{({ getRootProps, getInputProps }) => (
+		<>
+			{isLoading ? (
 				<Flex
-					{...getRootProps()}
 					justify="center"
 					position="fixed"
 					px={12}
@@ -31,26 +42,43 @@ export const ImageDropzone = ({
 					width="100%"
 					zIndex={999}
 				>
-					<Flex
-						align="center"
-						borderColor="black"
-						borderRadius={8}
-						borderStyle="dashed"
-						borderWidth={2}
-						direction="column"
-						gap={2}
-						px={8}
-						py={12}
-						width="full"
-					>
-						<input {...getInputProps()} />
-						<Icon height={8} as={SlPicture} width={8} />
-						<Text fontWeight="bold" textAlign="center">
-							drop image here, or click to select image.
-						</Text>
-					</Flex>
+					<Spinner size="lg" />
 				</Flex>
+			) : (
+				<Dropzone onDrop={handleOnDrop}>
+					{({ getRootProps, getInputProps }) => (
+						<Flex
+							{...getRootProps()}
+							justify="center"
+							position="fixed"
+							px={12}
+							top="50%"
+							transform="translateY(-50%)"
+							width="100%"
+							zIndex={999}
+						>
+							<Flex
+								align="center"
+								borderColor="black"
+								borderRadius={8}
+								borderStyle="dashed"
+								borderWidth={2}
+								direction="column"
+								gap={2}
+								px={8}
+								py={12}
+								width="full"
+							>
+								<input {...getInputProps()} />
+								<Icon height={8} as={SlPicture} width={8} />
+								<Text fontWeight="bold" textAlign="center">
+									drop image here, or click to select image.
+								</Text>
+							</Flex>
+						</Flex>
+					)}
+				</Dropzone>
 			)}
-		</Dropzone>
+		</>
 	);
 };
