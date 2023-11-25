ba8d87b97803e69784caaf90f2bfdb2c02ceaf77
takumi <hello.camiha@gmail.com>
2023-11-25 13:35:51 +0900

    using radix component

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,15 @@
-import { Flex, Text, Button, Slider } from "@radix-ui/themes";
+import { Flex, Heading, Text, Button, Slider } from "@radix-ui/themes";
 
 function App() {
 	return (
-		<div>
-			<h1>React App</h1>
+		<Flex direction="column" gap="4">
+			<Heading>React App</Heading>
 			<Flex direction="column" gap="2">
 				<Text>Hello from Radix Themes :)</Text>
 				<Button>Let's go</Button>
 				<Slider defaultValue={[50]} />
 			</Flex>
-		</div>
+		</Flex>
 	);
 }
 
