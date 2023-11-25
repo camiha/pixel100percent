b8e091531ead496835eb41f12e0599fe3e66dd38
takumi <hello.camiha@gmail.com>
2023-11-25 19:42:13 +0900

    remove unuse dom

--- src/components/image-dropzone.tsx
+++ src/components/image-dropzone.tsx
@@ -25,36 +25,35 @@ export const ImageDropzone = ({
 	return (
 		<Dropzone onDrop={handleOnDrop}>
 			{({ getRootProps, getInputProps }) => (
-				<div {...getRootProps()}>
+				<Flex
+					position="fixed"
+					justify="center"
+					top="50%"
+					width="100%"
+					transform="translateY(-50%)"
+					zIndex={999}
+					px={12}
+					{...getRootProps()}
+				>
 					<Flex
-						position="fixed"
-						justify="center"
-						top="50%"
-						width="100%"
-						transform="translateY(-50%)"
-						zIndex={999}
-						px={12}
+						direction="column"
+						align="center"
+						gap={2}
+						px={8}
+						py={12}
+						width="full"
+						borderRadius={8}
+						borderStyle="dashed"
+						borderWidth={2}
+						borderColor="#000"
 					>
-						<Flex
-							direction="column"
-							align="center"
-							gap={2}
-							px={8}
-							py={12}
-							width="full"
-							borderRadius={8}
-							borderStyle="dashed"
-							borderWidth={2}
-							borderColor="#000"
-						>
-							<input {...getInputProps()} />
-							<Icon as={SlPicture} width={8} height={8} />
-							<Text fontWeight="bold" textAlign="center">
-								drop files here, or click to select files.
-							</Text>
-						</Flex>
+						<input {...getInputProps()} />
+						<Icon as={SlPicture} width={8} height={8} />
+						<Text fontWeight="bold" textAlign="center">
+							drop files here, or click to select files.
+						</Text>
 					</Flex>
-				</div>
+				</Flex>
 			)}
 		</Dropzone>
 	);
