38f0eb759c7e721b6a88f94120a405ec1f878b04
takumi <hello.camiha@gmail.com>
2023-11-25 20:19:29 +0900

    comment out debugger

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ const createWindow = () => {
 	}
 
 	// Open the DevTools.
-	mainWindow.webContents.openDevTools();
+	// mainWindow.webContents.openDevTools();
 
 	mainWindow.setAlwaysOnTop(true, "floating");
 };
