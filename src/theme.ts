cb97236e32aec8f04f422bfdf2aad0c4a87a6209
takumi <hello.camiha@gmail.com>
2023-11-25 20:54:19 +0900

    migrate css to chakra

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,19 @@
-import { extendTheme } from "@chakra-ui/react";
+import { StyleFunctionProps, extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-	// fonts: {
-	// heading: `'Titillium Web', sans-serif`,
-	// body: `'Titillium Web', sans-serif`,
-	// },
+	styles: {
+		global: (props: StyleFunctionProps) => ({
+			body: {
+				margin: 0,
+				padding: 0,
+				bg: "rgba(0,0,0,0)",
+				"-webkit-user-drag": "none",
+			},
+			"::-webkit-scrollbar": {
+				display: "none",
+			},
+		}),
+	},
 });
 
 export default theme;
