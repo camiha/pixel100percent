218bba511bc52ec5adc33e5737d13904243d9a68
takumi <hello.camiha@gmail.com>
2023-11-26 22:03:55 +0900

    setup chakra

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<ChakraProvider theme={theme}>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<App />
+		</ChakraProvider>
+	</React.StrictMode>,
 );
